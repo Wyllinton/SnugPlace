5598d8ee2cb55fde0cf1ca7cd1dd7863bb6adbbc
Wyllinton <willinton.vergarac@uqvirtual.edu.co>
2025-08-24 00:03:20 -0500

    Initial commit: SnugPlace project with MariaDB connection

---
+++ src/main/java/com/snugplace/demo/SnugplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snugplace.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnugplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnugplaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snugplace/demo/SnugplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snugplace.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnugplaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
